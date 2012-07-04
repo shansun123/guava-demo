45e250618eaba290f1834cba1a7091393b59d87c
bo lan <lanbo.xj@gmail.com>
2012-07-04 15:11:35 +0800

    Add concurrency & eventbus & range & strings demo.

---
+++ src/main/java/com/shansun/demo/guava/concurrency/ServiceUsage.java
@@ -0,0 +1,57 @@
+package com.shansun.demo.guava.concurrency;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.Service;
+
+/**  
+ * <p></p> 
+ * @author:     lanbo <br>
+ * @version:    1.0  <br>
+ * @date:   	2012-7-4
+ */
+public class ServiceUsage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static class SimpleService implements Service {
+
+		boolean running = false;
+		
+		@Override
+		public ListenableFuture<State> start() {
+			return null;
+		}
+
+		@Override
+		public State startAndWait() {
+			return null;
+		}
+
+		@Override
+		public boolean isRunning() {
+			return false;
+		}
+
+		@Override
+		public State state() {
+			return null;
+		}
+
+		@Override
+		public ListenableFuture<State> stop() {
+			return null;
+		}
+
+		@Override
+		public State stopAndWait() {
+			return null;
+		}
+		
+	}
+}
